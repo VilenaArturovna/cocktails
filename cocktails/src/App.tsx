64e91066ea913a9f0118d35ff1e4f3acd3300215
VilenaArturovna <vilena-forever@inbox.ru>
2021-06-01 00:27:30 +0600

    added all files to github

---
+++ cocktails/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Main} from "./components/Main";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
+
+function App() {
+  return (
+    <div className="App">
+        <Provider store={store}><Main/></Provider>
+    </div>
+  );
+}
+
+export default App;
